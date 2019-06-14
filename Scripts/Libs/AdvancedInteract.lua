a2e9e7a2b185e54733739f849a6c9d821a0ea2a7
TechnologicNick <nickvanluijk@gmail.com>
2019-06-14 21:34:37 +0200

    Keys are working

---
+++ Scripts/Libs/AdvancedInteract.lua
@@ -0,0 +1,74 @@
+-- Libs/AdvancedInteract.lua --
+
+print("Libs/AdvancedInteract.lua reloaded")
+
+if not sm.advancedInteract then
+    sm.advancedInteract = {}
+end
+
+function sm.advancedInteract.create( parentClass )
+    local object = {}
+    
+    object.parentClass = parentClass
+    object.buttons = {}
+    
+    function object.addButton( self, name, x, y, width, height, effectName, onClick, onHover, active )
+        local btn = {}
+        
+        btn.name = name
+        btn.x = x
+        btn.y = y
+        btn.width = width
+        btn.height = height
+        btn.effectName = effectName
+        btn.onClick = onClick
+        btn.onHover = onHover
+        btn.active = active and true
+        
+        self.buttons[btn.name] = btn
+    end
+    
+    function object.getRaycastPoint( self )
+        local succes, raycastResult = sm.localPlayer.getRaycast(10)
+    
+        if succes then
+            local hitPos = raycastResult.pointWorld -- world point the vector hit
+            local worldPos = self.parentClass.shape:getWorldPosition() -- world point of block center
+            local localHitVec = hitPos - worldPos -- vector of hit relative to block
+            local localX = self.parentClass.shape:getRight()
+            local localY = self.parentClass.shape:getAt()
+            local localZ = self.parentClass.shape:getUp()
+            dotX = sm.vec3.dot(localHitVec, localX) * 4
+            dotY = sm.vec3.dot(localHitVec, localY) * 4
+            dotZ = sm.vec3.dot(localHitVec, localZ) * 4
+            
+            return dotX, dotY, dotZ
+        end
+        
+    end
+    
+    function object.onInteract( self )
+        local dotX, dotY, dotZ = self:getRaycastPoint()
+        
+        --print("Dot X: "..string.format("%.3f",dotX))
+        --print("Dot Y: "..string.format("%.3f",dotY))
+        --print("Dot Z: "..string.format("%.3f",dotZ))
+        
+        local btn = self:getButtonAtPosition(dotX, dotY)
+        if btn and btn.onClick then
+            btn.onClick(self.parentClass, btn)
+        end
+    end
+    
+    function object.getButtonAtPosition( self, x, y )
+        for name, btn in pairs(self.buttons) do
+            if btn.x <= x and x <= btn.x + btn.width  and
+               btn.y <= y and y <= btn.y + btn.height then
+               
+                return btn
+            end
+        end
+    end
+    
+    return object
+end
